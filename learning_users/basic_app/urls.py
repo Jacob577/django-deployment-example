497bcab6156b19c0724d6f3661ddd1874b40f473
Jacob577 <deltora.ericsson@gmail.com>
2020-08-05 18:25:10 +0200

    'first'

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls import url
+from basic_app import views
+
+#Template urls
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+]
